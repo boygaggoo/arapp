92789ba35574885ea849e33a907ae07aa03b11e8
Kunal Agarwal <kunal.agarwal@flipkart.com>
2014-08-08 05:08:11 +0530

    Added call for Category in Listing

--- src/com/fk/arapp/Listing.java
+++ src/com/fk/arapp/Listing.java
@@ -1,12 +1,17 @@
 package com.fk.arapp;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.fk.arapp.RestClient.RequestMethod;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 //import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,86 +20,113 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
+import android.os.StrictMode;
 
-
-public class Listing extends Activity implements SurfaceHolder.Callback{
+public class Listing extends Activity implements SurfaceHolder.Callback {
 
 	Camera camera;
 	SurfaceView surfaceView;
 	SurfaceHolder surfaceHolder;
 	boolean previewing = false;
 	LayoutInflater controlInflater = null;
-	
-    @SuppressWarnings("deprecation")
+	ArrayList<String> imageUrls = new ArrayList<String>();
+
+	@SuppressWarnings("deprecation")
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.listing_main);
-        
-        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        
-        getWindow().setFormat(PixelFormat.UNKNOWN);
-        surfaceView = (SurfaceView)findViewById(R.id.camerapreview);
-        surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        
-        controlInflater = LayoutInflater.from(getBaseContext());
-        View viewControl = controlInflater.inflate(R.layout.listing_control, null);
-        LayoutParams layoutParamsControl
-         = new LayoutParams(LayoutParams.FILL_PARENT,
-         LayoutParams.FILL_PARENT);
-        this.addContentView(viewControl, layoutParamsControl);
-        
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.listing_main);
+
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+					.permitAll().build();
+			StrictMode.setThreadPolicy(policy);
+		}
+
+		// setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+		getWindow().setFormat(PixelFormat.UNKNOWN);
+		surfaceView = (SurfaceView) findViewById(R.id.camerapreview);
+		surfaceHolder = surfaceView.getHolder();
+		surfaceHolder.addCallback(this);
+		surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+		controlInflater = LayoutInflater.from(getBaseContext());
+		View viewControl = controlInflater.inflate(R.layout.listing_control,
+				null);
+		LayoutParams layoutParamsControl = new LayoutParams(
+				LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+		this.addContentView(viewControl, layoutParamsControl);
+
+	}
+
+	
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		RestClient client = new RestClient(
+				"http://sleepy-retreat-2061.herokuapp.com/products");
+		client.AddParam("category", "watches");
+
+		try {
+			client.Execute(RequestMethod.GET);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String response = client.getResponse();
+
+		
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	@Override
 	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-		if(previewing){
-			 camera.stopPreview();
-			 previewing = false;
-			}
+		if (previewing) {
+			camera.stopPreview();
+			previewing = false;
+		}
 
-			if (camera != null){
-			 try {
-			  camera.setPreviewDisplay(surfaceHolder);
-			  camera.startPreview();
-			  previewing = true;
-			 } catch (IOException e) {
-			  // TODO Auto-generated catch block
-			  e.printStackTrace();
-			 }
+		if (camera != null) {
+			try {
+				camera.setPreviewDisplay(surfaceHolder);
+				camera.startPreview();
+				previewing = true;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+		}
 	}
-
 
 	@Override
 	public void surfaceCreated(SurfaceHolder arg0) {
 		camera = Camera.open();
 	}
-
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder arg0) {
