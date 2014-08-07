d9afcd44aec3dc20403a2de734828adb4d71360e
Sathish <sathish316@gmail.com>
2014-08-08 01:53:37 +0530

    home activity

--- gen/com/fk/arapp/R.java
+++ gen/com/fk/arapp/R.java
@@ -10,6 +10,11 @@ package com.fk.arapp;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        /**  colors 
+         */
+        public static final int background=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -25,17 +30,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090003;
+        public static final int cat_shades_button=0x7f090001;
+        public static final int cat_shoes_button=0x7f090002;
+        public static final int cat_watches_button=0x7f090000;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
+        public static final int home=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        /**  app labels 
+         */
+        public static final int arapp_title=0x7f050003;
+        public static final int cat_shades_label=0x7f050005;
+        public static final int cat_shoes_label=0x7f050006;
+        /**  categories 
+         */
+        public static final int cat_watches_label=0x7f050004;
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
@@ -60,10 +76,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
